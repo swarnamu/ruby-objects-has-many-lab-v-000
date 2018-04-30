e2edd18e322283f8f92d860242633961593efa9e
Swarna Muralidharan <swarna.muralic@gmail.com>
2018-04-30 19:40:48 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,12 +14,14 @@ class Artist
   def add_songs(song)
     self.songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     self.songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def self.song_count
