0a05fd9b853d34d5a55e1ee3a8c7526f73a4ce89
Swarna Muralidharan <swarna.muralic@gmail.com>
2018-04-30 20:19:34 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -18,4 +18,11 @@ class Author
     @@post_count += 1
   end
 
+  def add_post_by_title(title)
+    post = Post.new(title)
+    self.posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
 end
